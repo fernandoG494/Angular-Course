bc76aa3b36bcf041169c1ae2e653b5d23e89f5c3
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-13 12:23:03 -0600

    NgModel two way data binding

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000,
+  };
+
   agregar() {
-    console.log('Hey');
+    console.log(this.nuevo);
   }
 }
