611afcbc6a73efe4e65d8ea2bf9aa016d9bf7f99
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-13 15:14:37 -0600

    Service implementation

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  personajes: Personaje[] = [
-    { nombre: 'Goku', poder: 15000 },
-    { nombre: 'Vegeta', poder: 10000 },
-  ];
+  constructor() {}
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 123123,
   };
 
-  agregarNuevoPersonaje(argumento: Personaje) {
-    this.personajes.push(argumento);
-  }
+  agregarNuevoPersonaje(argumento: Personaje) {}
 }
---
+++ 01-bases/src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Injectable()
+export class DbzService {
+  constructor() {}
+
+  private _personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 15000 },
+    { nombre: 'Vegeta', poder: 10000 },
+  ];
+
+  get personajes() {
+    return [...this._personajes];
+  }
+
+  agregarPersonaje(personaje: Personaje) {
+    this._personajes.push(personaje);
+  }
+}
