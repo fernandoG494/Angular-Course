fd65eb693833196d17042870bca236c2eba587d1
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-13 12:55:21 -0600

    Add DBZ character and number pipe

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -10,12 +10,22 @@ interface Personaje {
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 15000 },
+    { nombre: 'Vegeta', poder: 10000 },
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000,
+    nombre: '',
+    poder: 0,
   };
 
   agregar() {
-    console.log(this.nuevo);
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = { nombre: '', poder: 0 };
   }
 }
