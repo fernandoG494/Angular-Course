ad2c21f0d7286a5d655522dccf4b88df329383c7
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-14 11:25:33 -0600

    Gifs app initial commit

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DbzModule } from './dbz/dbz.module';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
-import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
