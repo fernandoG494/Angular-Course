49cb9b14c68230c438159a09ae10899fbfe5b8f7
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-14 15:07:24 -0600

    10 gifs service implemented

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [SharedModule, BrowserModule, AppRoutingModule, GifsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent {
+  @ViewChild('textBuscar') textBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifService: GifsService) {}
+
+  buscar() {
+    const valor = this.textBuscar.nativeElement.value;
+
+    this.gifService.buscarGifs(valor);
+
+    this.textBuscar.nativeElement.value = '';
+  }
+}
