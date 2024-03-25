8795f3a1cf18e109c8b02eef21af69c3549eccc5
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-24 23:51:56 -0600

    region apge changes

--- 03-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 03-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,8 +13,12 @@ export class ByRegionPageComponent {
   constructor(private countriesServices: CountriesService) {}
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.countriesServices.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
     });
--- 03-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 03-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,21 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input() public placeholder: string = '';
   @Output() public onValue = new EventEmitter<string>();
   @Output() public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer.pipe(debounceTime(1000)).subscribe((value) => {
-      this.onDebounce.emit(value);
-    });
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
   }
 
   emitValue(value: string): void {
