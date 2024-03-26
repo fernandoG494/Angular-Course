157cda9cde2b418b0544f0af6ff4155929f11718
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-25 20:27:06 -0600

    PrimeNG menu bar

---
+++ 04-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``,
+})
+export class MenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes deAngular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          },
+        ],
+      },
+    ];
+  }
+}
