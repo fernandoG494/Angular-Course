4b04a9c27f7138c7d488d6dcbae0fc20382e48b7
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-03-27 10:13:19 -0600

    Heroes table

--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { ToolbarModule } from 'primeng/toolbar';
 import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
@@ -13,6 +15,8 @@ import { FieldsetModule } from 'primeng/fieldset';
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    TableModule,
+    ToolbarModule,
   ],
 })
 export class PrimeNgModule {}
---
+++ 04-pipesApp/src/app/products/pages/order/order.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styles: ``,
+})
+export class OrderComponent {
+  public isUpperCase: boolean = false;
+
+  toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  public heroes: Hero[] = [
+    { name: 'Superman', canFly: true, color: Color.blue },
+    { name: 'Batman', canFly: false, color: Color.black },
+    { name: 'Daredevil', canFly: false, color: Color.red },
+    { name: 'Robin', canFly: false, color: Color.green },
+  ];
+}
