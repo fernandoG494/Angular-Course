6f3e490961135e544fbf79b81c6664d70b10a59a
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-01 06:35:30 -0600

    First views implemented

--- 05-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
+++ 05-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class LoginPageComponent {
-
-}
+export class LoginPageComponent {}
