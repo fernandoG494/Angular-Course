7c2fdc9d7add8c7fc5f71ba9ee30134595079834
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-01 07:23:05 -0600

    Page implementation

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
