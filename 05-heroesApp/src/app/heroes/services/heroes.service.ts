e6cc06c0e93925318c1b3b0425ac288a0dfad518
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-01 17:55:41 -0600

    hero page implemented

--- 05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environment';
 
@@ -12,5 +13,11 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 }
