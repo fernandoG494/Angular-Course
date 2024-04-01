5fd4fa8fc4aed428e28f169eac8b81d7abd3f18d
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-01 17:26:11 -0600

    Hero pipe implemented

--- 05-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 05-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -6,6 +6,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
+// localhost:4200/heroes
 const routes: Routes = [
   {
     path: '',
@@ -13,7 +14,7 @@ const routes: Routes = [
     children: [
       { path: 'new-hero', component: NewPageComponent },
       { path: 'search', component: SearchPageComponent },
-      { path: 'edit:/id', component: NewPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
       { path: 'list', component: ListPageComponent },
       { path: ':id', component: HeroPageComponent },
       { path: '**', redirectTo: 'list' },
--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
-import { NewPageComponent } from './pages/new-page/new-page.component';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 import { MaterialModule } from '../material/material.module';
+import { HeroesRoutingModule } from './heroes-routing.module';
 import { CardComponent } from './components/card/card.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
+    CardComponent,
+    HeroImagePipe,
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
-    CardComponent,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
