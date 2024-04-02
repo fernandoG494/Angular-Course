c207f4bca2c69dfa206cd52a5e1da80a5c8e4a32
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-01 21:07:34 -0600

    Search field implemented

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,26 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { HeroImagePipe } from './pipes/hero-image.pipe';
-import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { CardComponent } from './components/card/card.component';
-import { NewPageComponent } from './pages/new-page/new-page.component';
+import { MaterialModule } from '../material/material.module';
+
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
   declarations: [
-    CardComponent,
-    HeroImagePipe,
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
+
+    // Pipes
+    HeroImagePipe,
   ],
-  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HeroesRoutingModule,
+    MaterialModule,
+  ],
 })
 export class HeroesModule {}
