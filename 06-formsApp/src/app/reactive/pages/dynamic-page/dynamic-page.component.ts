6f2ad6fe375aa1543048a08d08b89ed57c9d40cd
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-09 14:01:58 -0600

    Dinamic form implemented

--- 06-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: ``,
+  styles: [],
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', Validators.required, Validators.minLength(3)],
+    favoriteGames: this.formBuilder.array([
+      ['Death Stranding', Validators.required],
+      ['Metal Gear Solid', Validators.required],
+    ]),
+  });
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+}
