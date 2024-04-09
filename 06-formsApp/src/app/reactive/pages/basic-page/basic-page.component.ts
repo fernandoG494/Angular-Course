c058d1cf8f6d675f1f333a06880f51f76a5c4629
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-09 13:05:05 -0600

    re start form values

--- 06-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const productRTX = {
+  name: 'RTX 5060',
+  price: 2500,
+  inStorage: 0,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent {
-  // public myForm: FormGroup = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   isStorage: new FormControl(0),
-  // });
-
+export class BasicPageComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
 
   public myForm: FormGroup = this.formBuilder.group({
@@ -20,9 +20,15 @@ export class BasicPageComponent {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  ngOnInit(): void {
+    this.myForm.reset(productRTX);
+  }
+
   onSave(): void {
     if (this.myForm.invalid) return;
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
