8e62aa2e841262cb1c5e4a93265ebfa15b18f234
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-15 13:57:26 -0600

    Validations base

--- 06-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 06-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``,
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+
+  isValidField() {
+    // TODO: obtener validaciones de servicio
+  }
+}
