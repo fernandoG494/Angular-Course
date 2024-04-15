d49f9a0a374ec09633f44dcb0f640a2dd8957bf0
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-15 13:42:01 -0600

    Switch reactive solution

--- 06-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``,
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  public myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+}
